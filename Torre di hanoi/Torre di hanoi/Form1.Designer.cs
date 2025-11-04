ae4aef6bcc44cf28b62ba3694aa2d5d1813a82b6
12999355@itisrossi.vi.it <12999355@itisrossi.vi.it>
2025-11-04 23:21:11 +0100

    finita Creazione  degli elementi

--- Torre di hanoi/Torre di hanoi/Form1.Designer.cs
+++ Torre di hanoi/Torre di hanoi/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Pnl_Base = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.Pnl_Base1 = new System.Windows.Forms.Panel();
+            this.Pnl_Base2 = new System.Windows.Forms.Panel();
+            this.Pnl_Base3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
@@ -42,57 +42,64 @@
             this.panel7.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Pnl_Base
+            // Pnl_Base1
             // 
-            this.Pnl_Base.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.Pnl_Base.Location = new System.Drawing.Point(70, 300);
-            this.Pnl_Base.Name = "Pnl_Base";
-            this.Pnl_Base.Size = new System.Drawing.Size(150, 15);
-            this.Pnl_Base.TabIndex = 0;
+            this.Pnl_Base1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Pnl_Base1.Location = new System.Drawing.Point(93, 369);
+            this.Pnl_Base1.Margin = new System.Windows.Forms.Padding(4);
+            this.Pnl_Base1.Name = "Pnl_Base1";
+            this.Pnl_Base1.Size = new System.Drawing.Size(200, 18);
+            this.Pnl_Base1.TabIndex = 0;
             // 
-            // panel2
+            // Pnl_Base2
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel2.Location = new System.Drawing.Point(250, 300);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(150, 15);
-            this.panel2.TabIndex = 1;
+            this.Pnl_Base2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Pnl_Base2.Location = new System.Drawing.Point(333, 369);
+            this.Pnl_Base2.Margin = new System.Windows.Forms.Padding(4);
+            this.Pnl_Base2.Name = "Pnl_Base2";
+            this.Pnl_Base2.Size = new System.Drawing.Size(200, 18);
+            this.Pnl_Base2.TabIndex = 1;
             // 
-            // panel3
+            // Pnl_Base3
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel3.Location = new System.Drawing.Point(430, 300);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(150, 15);
-            this.panel3.TabIndex = 2;
+            this.Pnl_Base3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Pnl_Base3.Location = new System.Drawing.Point(573, 369);
+            this.Pnl_Base3.Margin = new System.Windows.Forms.Padding(4);
+            this.Pnl_Base3.Name = "Pnl_Base3";
+            this.Pnl_Base3.Size = new System.Drawing.Size(200, 18);
+            this.Pnl_Base3.TabIndex = 2;
             // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel4.Location = new System.Drawing.Point(138, 115);
+            this.panel4.Location = new System.Drawing.Point(184, 188);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(15, 200);
+            this.panel4.Size = new System.Drawing.Size(20, 200);
             this.panel4.TabIndex = 1;
             // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel5.Location = new System.Drawing.Point(318, 115);
+            this.panel5.Location = new System.Drawing.Point(424, 188);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(15, 200);
+            this.panel5.Size = new System.Drawing.Size(20, 200);
             this.panel5.TabIndex = 2;
             // 
             // panel6
             // 
             this.panel6.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel6.Location = new System.Drawing.Point(498, 115);
+            this.panel6.Location = new System.Drawing.Point(664, 188);
+            this.panel6.Margin = new System.Windows.Forms.Padding(4);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(15, 200);
+            this.panel6.Size = new System.Drawing.Size(20, 200);
             this.panel6.TabIndex = 3;
             // 
             // sel_Rings
             // 
-            this.sel_Rings.Location = new System.Drawing.Point(43, 47);
+            this.sel_Rings.Location = new System.Drawing.Point(57, 58);
+            this.sel_Rings.Margin = new System.Windows.Forms.Padding(4);
             this.sel_Rings.Maximum = new decimal(new int[] {
             7,
             0,
@@ -104,7 +111,7 @@
             0,
             0});
             this.sel_Rings.Name = "sel_Rings";
-            this.sel_Rings.Size = new System.Drawing.Size(150, 20);
+            this.sel_Rings.Size = new System.Drawing.Size(200, 22);
             this.sel_Rings.TabIndex = 4;
             this.sel_Rings.Value = new decimal(new int[] {
             1,
@@ -114,9 +121,10 @@
             // 
             // btn_Start
             // 
-            this.btn_Start.Location = new System.Drawing.Point(43, 73);
+            this.btn_Start.Location = new System.Drawing.Point(57, 90);
+            this.btn_Start.Margin = new System.Windows.Forms.Padding(4);
             this.btn_Start.Name = "btn_Start";
-            this.btn_Start.Size = new System.Drawing.Size(150, 20);
+            this.btn_Start.Size = new System.Drawing.Size(200, 25);
             this.btn_Start.TabIndex = 5;
             this.btn_Start.Text = "Inizia";
             this.btn_Start.UseVisualStyleBackColor = true;
@@ -128,32 +136,35 @@
             this.panel7.Controls.Add(this.label1);
             this.panel7.Controls.Add(this.btn_Start);
             this.panel7.Controls.Add(this.sel_Rings);
-            this.panel7.Location = new System.Drawing.Point(711, 76);
+            this.panel7.Location = new System.Drawing.Point(948, 94);
+            this.panel7.Margin = new System.Windows.Forms.Padding(4);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(241, 111);
+            this.panel7.Size = new System.Drawing.Size(321, 137);
             this.panel7.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Location = new System.Drawing.Point(4, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(238, 30);
+            this.label1.Size = new System.Drawing.Size(317, 37);
             this.label1.TabIndex = 6;
             this.label1.Text = "SELEZIONA IL  NUMERO DI ANELLI";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(981, 450);
+            this.ClientSize = new System.Drawing.Size(1308, 554);
             this.Controls.Add(this.panel7);
             this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.Pnl_Base);
+            this.Controls.Add(this.Pnl_Base2);
+            this.Controls.Add(this.Pnl_Base3);
+            this.Controls.Add(this.Pnl_Base1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.sel_Rings)).EndInit();
@@ -164,9 +175,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel Pnl_Base;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel Pnl_Base1;
+        private System.Windows.Forms.Panel Pnl_Base2;
+        private System.Windows.Forms.Panel Pnl_Base3;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Panel panel6;
--- Torre di hanoi/Torre di hanoi/Form1.cs
+++ Torre di hanoi/Torre di hanoi/Form1.cs
@@ -15,14 +15,11 @@ namespace Torre_di_hanoi
         public Form1()
         {
             InitializeComponent();
-            YIniziale = Pnl_Base.Location.Y - 15;
         }
 
         Color[] Listacolori = { Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Indigo, Color.Purple };
         int i = 0;
-        int length = 130;
-        int XIniziale = 50;
-        int YIniziale;
+        int length; 
 
         List<Panel> Primalista = new List<Panel>();
         List<Panel> Secondalista = new List<Panel>();
@@ -30,37 +27,79 @@ namespace Torre_di_hanoi
 
         private void btn_Start_Click(object sender, EventArgs e)
         {
+            Resetgame();
+
             int nDischi = (int)sel_Rings.Value;
 
-            for(int i = 0; i < nDischi; i++)
+            //vado a fare si che ci sia sempre il pio piccolo in cima
+            //tolgo al valore massimo il valore dei dischi cha non ci sono nella piramide
+            length = 140 - (18 * (7-nDischi));  
+
+            for (int i = 0; i < nDischi; i++)
             {
-                CalculateX();
-                CreatePanel(XIniziale, YIniziale);
-                YIniziale -= 15;
+                CreatePanel(Pnl_Base1);
             }
             
         }
 
-        private void CreatePanel(int x, int y)
+        private void CreatePanel(Panel PannelloBase)
         {
             Panel DiscoNuovo = new Panel();
+
             DiscoNuovo.Height = 15;
             DiscoNuovo.Size = new Size(length, 15);
+
             DiscoNuovo.BackColor = Listacolori[i];
-            DiscoNuovo.Location = new Point(x, y);
+            DiscoNuovo.Location = new Point(CalculateX(DiscoNuovo, PannelloBase ), CalculateY(PannelloBase, Primalista));
             
-            length -= 15;
+            length -= 18;
             i = i < 6 ? i+1 : 0;
+
             //aggiungo il pannello istanziato nel form
             Controls.Add(DiscoNuovo);
             DiscoNuovo.BringToFront();
+            //gli assegnam oun nome e li mettiamo in una lista
             DiscoNuovo.Name = $"pnl_{i+1}";
             Primalista.Add(DiscoNuovo);
         }
 
-        private void CalculateX()
+        private int CalculateX(Panel MovedPanel, Panel BasePanel)
         {
-            XIniziale = Pnl_Base.Location.X + ((Pnl_Base.Width - length) / 2);
+            return BasePanel.Location.X + ((BasePanel.Width - MovedPanel.Width) / 2);
+        }
+
+        private int CalculateY( Panel BasePanel, List<Panel> Lista)
+        {
+            int y = BasePanel.Location.Y;
+            y -= 15;
+            foreach (var element in Lista)
+            {
+                y -= 15;
+            }
+            return y;
+        }
+
+        private void Resetgame()
+        {
+            foreach (var element in Primalista)
+            {
+                element.Dispose();
+            }
+            
+            foreach (var element in Secondalista)
+            {
+                element.Dispose();
+            }
+
+            foreach (var element in Terzalista)
+            {
+                element.Dispose();
+            }
+
+            Primalista.Clear();
+            Primalista.Clear();
+            Primalista.Clear();
+            i = 0;
         }
     }
 }
